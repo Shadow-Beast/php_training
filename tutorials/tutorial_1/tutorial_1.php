43a165d8ce1a41b2227c0eabce56f90243bbb50f
Zaw Win Tin <zawwintin21@gmail.com>
2021-10-15 10:53:13 +0630

    [master] Rewrite Tutorial_1 with Coding Standard

--- tutorials/tutorial_1/tutorial_1.php
+++ tutorials/tutorial_1/tutorial_1.php
@@ -1,20 +1,34 @@
 <?php
-    echo '<link rel="stylesheet" href="css/reset.css" />';
-    echo '<link rel="stylesheet" href="css/style.css" />';
-
-    $max_rows = 8;
-    $max_columns = 8;    
-    
-    echo '<table class="chess_table" cellspacing="0" cellpadding="0">';
-    for ($row = 0; $row < $max_rows; $row++) {
-        echo '<tr>';
-        for ($col = 0; $col < $max_columns; $col++) {
-            if (($row+$col)%2==0) {
-                echo '<td class="white_square"></td>';
-            } else {
-                echo '<td class="black_square"></td>';
-            }
-        }
-        echo '</tr>';
+    /**
+     * Import css files from css folder
+     */
+    function importCSS() {
+        echo '<link rel="stylesheet" href="css/reset.css" />';
+        echo '<link rel="stylesheet" href="css/style.css" />';
     }
+
+    /**
+     * Create chessboard
+     */
+    function createChessBoard() {
+        $max_rows = 8;
+        $max_columns = 8;    
+        
+        echo '<table class="chess_table" cellspacing="0" cellpadding="0">';
+        for ($row = 0; $row < $max_rows; $row++) {
+            echo '<tr>';
+            for ($col = 0; $col < $max_columns; $col++) {
+                if (($row+$col)%2==0) {
+                    echo '<td class="white_square"></td>';
+                } else {
+                    echo '<td class="black_square"></td>';
+                }
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+
+    importCSS();
+    createChessBoard();
 ?>

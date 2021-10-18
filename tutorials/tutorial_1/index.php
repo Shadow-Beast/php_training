dd988cf50d2004cbb8c4e1912055aa6034e5f46d
Zaw Win Tin <zawwintin21@gmail.com>
2021-10-18 23:39:56 +0630

    [master] Change Doc function in Tutorial 5

---
+++ tutorials/tutorial_1/index.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+     * Import css files from css folder
+     */
+    function importCSS() {
+        echo '<link rel="stylesheet" href="css/reset.css" />';
+        echo '<link rel="stylesheet" href="css/style.css" />';
+    }
+
+    /**
+     * Create chessboard
+     * 
+     * @param $max_rows
+     * @param $max_columns
+     */
+    function createChessBoard($max_rows, $max_columns) {        
+        echo '<table class="chess_table" cellspacing="0" cellpadding="0">';
+        for ($row = 0; $row < $max_rows; $row++) {
+            echo '<tr>';
+            for ($col = 0; $col < $max_columns; $col++) {
+                if ( ($row+$col)%2 == 0) {
+                    echo '<td class="white_square"></td>';
+                } else {
+                    echo '<td class="black_square"></td>';
+                }
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+
+    importCSS();
+    createChessBoard(8, 8);
+?>

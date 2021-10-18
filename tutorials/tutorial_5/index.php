d97d18aa0ca11a694ce9659888184fd63af3fbf0
Zaw Win Tin <zawwintin21@gmail.com>
2021-10-18 20:58:17 +0630

    [master] Fix file reading error in Tutorial 5

--- tutorials/tutorial_5/index.php
+++ tutorials/tutorial_5/index.php
@@ -30,10 +30,81 @@
             if (!empty($_GET["filename"])) {
                 echo "<div class='file-block'>";
                 echo "<h2>",$_GET["filename"],"</h2><p>";
-                $filedir = $dir . $_GET["filename"];
-                $fileread= file_get_contents($filedir);
-                echo nl2br($fileread);
+                $file_dir = $dir . $_GET["filename"];
+                $file_extension = explode(".", $_GET["filename"])[1];
+                
+                if ($file_extension == "doc" || $file_extension == "txt") {
+                    readNormalTextFile($file_dir);
+                } elseif ($file_extension == "csv") {
+                    readCSVFile($file_dir);
+                } elseif ($file_extension == "xlsx") {
+                    readXLSXFile($file_dir);
+                } else {
+                    echo "Error File Format !";
+                }
+
                 echo "</p></div>";
+            }
+
+            /**
+             * Read file with doc, txt extension
+             */
+            function readNormalTextFile($file_dir)
+            {
+                $file_read= file_get_contents($file_dir);
+                echo nl2br($file_read);
+            }
+
+            /**
+             * Read file with csv extension
+             */
+            function readCSVFile($file_dir)
+            {
+                $csv_file = fopen($file_dir, "r");
+                echo "<table class='excel-table'>";
+                $row = 0;
+                while (!feof($csv_file)) {
+                    echo "<tr>";
+                    $data_in_row = fgetcsv($csv_file);
+                    //Check Array null or not
+                    if (is_array($data_in_row) || is_object($data_in_row)) {
+                        foreach ($data_in_row as $data) {
+                            if ($row == 0) {
+                                echo "<th>",$data,"</th>";
+                            } else {
+                                echo "<td>",$data,"</td>";
+                            }
+                        }
+                    }
+                    echo "</tr>";
+                    $row++;
+                }
+                echo "</table>";
+                fclose($csv_file);
+            }
+            
+            /**
+             * Read file with xlsx extension
+             */
+            function readXLSXFile($file_dir)
+            {
+                include "library/SimpleXLSX.php";
+                if ($xlsx = SimpleXLSX::parse($file_dir)) {
+                    echo "<table class='excel-table'>";
+                    $row = 0;
+                
+                    foreach ($xlsx->rows() as $elt) {
+                        if ($row == 0) {
+                            echo "<tr><th>" . $elt[0] . "</th><th>" . $elt[1] . "</th></tr>";
+                        } else {
+                            echo "<tr><td>" . $elt[0] . "</td><td>" . $elt[1] . "</td></tr>";
+                        }
+                        $row++;
+                    }
+                    echo "</table>";
+                } else {
+                    echo SimpleXLSX::parseError();
+                }
             }
         ?>
     </div>    

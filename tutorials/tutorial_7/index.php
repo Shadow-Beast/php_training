3d065a41234f40ae5ec92ecf7e6b551ee415a2d0
Zaw Win Tin <zawwintin21@gmail.com>
2021-10-19 17:25:38 +0630

    [master] Add Tutorial 7

---
+++ tutorials/tutorial_7/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial 7</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <form class="qr-form" action="php/displayqr.php" method="post">
+        <fieldset>            
+            <legend>QRCode Generator</legend>
+            <?php
+                //When Clicked without no data
+                if(!empty($_GET["nodata"])) {
+                    echo "<p class='warning-txt'>Please Enter data you want to generate !</p>";
+                }
+            ?>
+            <textarea name="data" id="data-txt" placeholder="Enter your data here ..."></textarea>
+            <input class="generate-btn" type="submit" value="Generate">
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ tutorials/tutorial_7/php/displayqr.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <?php
+        if(!empty($_POST["data"])) {
+            include "../library/phpqrcode/qrlib.php";
+            $txt_data = $_POST["data"];
+
+            //file image naming
+            $qrcode_id = 0;
+            $qrcode_folder_location = "../qr_img/";
+            //file path
+            $qrcode_img_location = $qrcode_folder_location . sprintf("%03d", $qrcode_id) . ".png";
+            while (file_exists($qrcode_img_location)) {
+                $qrcode_id++;
+                $qrcode_img_location = $qrcode_folder_location . sprintf("%03d", $qrcode_id) . ".png";
+            }
+
+            //other parameters
+            $ecc = 'H';
+            $pixel_size = 12;
+            $frame_size = 5;
+            QRcode::png($txt_data, $qrcode_img_location, $ecc, $pixel_size, $frame_size);
+
+            echo "<div class= 'qrcode-block'><h1>QR Code Generated Successfully!</h1>";
+            echo "<img src='" . $qrcode_img_location . "'>";
+            echo "<a class='goback-btn' href='../index.php'>Generate New Code</a></div>";
+        }
+        else {
+            header ("location: ../index.php?nodata=1");
+        }
+    ?>
+</body>
+</html>
